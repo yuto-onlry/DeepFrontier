5ca0e36ad3303439a761bd1515e17dd791ba3007
yuto-onlry <yuto_111255@icloud.com>
2026-06-26 11:32:55 +0900

    EenmyAnimation

--- DeepFrontier/AnimationPreset.cpp
+++ DeepFrontier/AnimationPreset.cpp
@@ -29,25 +29,10 @@ void AnimationPreset::SetAnimationPlayer(AnimationManager& animManager, int mode
 void AnimationPreset::SetAnimationLittleEnemy(AnimationManager& animManager, int modelHandle)
 {
     animManager.Init(modelHandle);
-	if (animManager.LoadAnimModel("../Animation/Enemy/StrutWalking.mv1") == true ||
-		animManager.LoadAnimModel("../Animation/Player/PlayerAnimation2.mv1") == true)
+	if (animManager.LoadAnimModel("../Animation/Enemy/Ran.mv1") == true)	
 	{
 		// アニメーション番号を指定+割り振り
-		animManager.SetAnimIndex(AnimationType::Idle, 0);
-		animManager.SetAnimIndex(AnimationType::Walk, 42);
-		animManager.SetAnimIndex(AnimationType::Run, 36);
-		animManager.SetAnimIndex(AnimationType::Attack, 39);
-		animManager.SetAnimIndex(AnimationType::StrongAttack, 12);
-		animManager.SetAnimIndex(AnimationType::AttackLockOn, 40);
-		animManager.SetAnimIndex(AnimationType::JumpStart, 16);
-		animManager.SetAnimIndex(AnimationType::JumpLoop, 15);
-		animManager.SetAnimIndex(AnimationType::JumpEnd, 14);
-		animManager.SetAnimIndex(AnimationType::Avoid, 27);
-		animManager.SetAnimIndex(AnimationType::Damage, 1);
-		animManager.SetAnimIndex(AnimationType::GetUp, 16);
-		animManager.SetAnimIndex(AnimationType::Dead, 8);
-
-		animManager.ChangeAnim(AnimationType::Idle);
+		animManager.SetAnimIndex(AnimationType::Run, 0);
 	}
 
 }
